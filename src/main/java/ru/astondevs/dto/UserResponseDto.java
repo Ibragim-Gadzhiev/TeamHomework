cba1132fbe61f15f9891f9bce469eee2c3b6cf81
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-04-30 18:48:40 +0300

    feat(dto): extend UserResponseDto with fileds

--- src/main/java/ru/astondevs/dto/UserResponseDto.java
+++ src/main/java/ru/astondevs/dto/UserResponseDto.java
@@ -1,4 +1,51 @@
 package ru.astondevs.dto;
 
+import java.time.LocalDateTime;
+
 public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    void setId(Long id) {
+        this.id = id;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setEmail(String email) {
+        this.email = email;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
