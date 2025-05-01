eb76af4f20d84262d24d3b844c76463dcac6f529
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-01 16:34:21 +0300

    feat(exception): add custom ResourceNotFoundException for error handling

--- src/main/java/ru/astondevs/exception/ResourceNotFoundException.java
+++ src/main/java/ru/astondevs/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package ru.astondevs.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
