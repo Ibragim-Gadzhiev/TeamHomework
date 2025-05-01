8ded35cf7a208e76fdb328f8c24ac4d6375e289c
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-01 14:57:05 +0300

    feat(exception): add initial DuplicateEmailException

---
+++ src/main/java/ru/astondevs/exception/DuplicateEmailException.java
@@ -0,0 +1,7 @@
+package ru.astondevs.exception;
+
+public class DuplicateEmailException extends RuntimeException {
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
+}
