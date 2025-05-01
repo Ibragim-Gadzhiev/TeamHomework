86479a7d55153b236776d8e1d5cd06652b3c2acd
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-01 16:20:15 +0300

    refactor(dto): convert UserCreateDto to record

--- src/main/java/ru/astondevs/dto/UserCreateDto.java
+++ src/main/java/ru/astondevs/dto/UserCreateDto.java
@@ -5,68 +5,40 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
 /**
  * DTO для создания нового пользователя.
  *
  * <p>Используется для передачи данных при создании нового пользователя</p>
+ *
+ * <p>Содержит следующие поля:
  * <ul>
- *     <li><b>Имя</b> – должно быть 2-50 символов, не должно быть пустым</li>
- *     <li><b>Email</b> – должен быть корректным и не быть пустым</li>
- *     <li><b>Возраст</b> от 0 до 120 лет</li>
+ *     <li>{@code name} – имя пользователя (должно быть 2-50 символов, не должно быть пустым)</li>
+ *     <li>{@code email} – адрес электронной почты (должен быть корректным и не быть пустым)</li>
+ *     <li>{@code age} – возраст (от 0 до 120 лет)</li>
  * </ul>
  */
-public class UserCreateDto {
-    @NotBlank(message = "{name.not.found}")
-    @Size(min = 2, max = 50, message = "{name.incorrect}")
-    private String name;
+@Builder
+public record UserCreateDto(
+        /**
+         * Имя пользователя(2-50 символов).
+         */
+        @NotBlank(message = "{name.not.found}")
+        @Size(min = 2, max = 50, message = "{name.incorrect}")
+        String name,
 
-    @NotBlank(message = "{email.not.found}")
-    @Email(message = "{email.incorrect}")
-    private String email;
+        /**
+         * Адрес электронной почты пользователя.
+         */
+        @NotBlank(message = "{email.not.found}")
+        @Email(message = "{email.incorrect}")
+        String email,
 
-    @Min(value = 0, message = "{min.age}") @Max(value = 120, message = "{max.age}")
-    private int age;
-
-    /**
-     * Возвращает имя пользователя.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Устанавливает имя пользователя.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Возвращает адрес электронной почты пользователя.
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Устанавливает адрес электронной почты пользователя.
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Возвращает возраст пользователя.
-     */
-    public int getAge() {
-        return age;
-    }
-
-    /**
-     * Устанавливает возраст пользователя.
-     */
-    public void setAge(int age) {
-        this.age = age;
-    }
+        /**
+         * Возраст пользователя(0-120 символов).
+         */
+        @Min(value = 0, message = "{min.age}") @Max(value = 120, message = "{max.age}")
+        int age
+) {
 }
