37058cefa07ae384c655b2fc922de8d71482e85d
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-01 16:20:45 +0300

    refactor(dto): convert UserUpdateDto to record

--- src/main/java/ru/astondevs/dto/UserUpdateDto.java
+++ src/main/java/ru/astondevs/dto/UserUpdateDto.java
@@ -1,10 +1,11 @@
 package ru.astondevs.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
 /**
  * DTO для обновления информации о пользователе.
@@ -16,57 +17,27 @@ import jakarta.validation.constraints.Size;
  *     <li>{@code age} – возраст (от 0 до 120 лет)</li>
  * </ul>
  */
-public class UserUpdateDto {
-    @NotBlank(message = "{name.not.found}")
-    @Size(min = 2, max = 50, message = "{name.incorrect}")
-    private String name;
+@Builder
+public record UserUpdateDto(
+        /**
+         * Имя пользователя.
+         */
+        @Nullable
+        @Size(min = 2, max = 50, message = "{name.incorrect}")
+        String name,
 
-    @NotBlank(message = "{email.not.found}")
-    @Email(message = "{email.incorrect}")
-    private String email;
+        /**
+         * Адрес электронной почты пользователя.
+         */
+        @Nullable
+        @Email(message = "{email.incorrect}")
+        String email,
 
-    @Min(value = 0, message = "{min.age}") @Max(value = 120, message = "{max.age}")
-    private int age;
-
-    /**
-     * Возвращает имя пользователя.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Устанавливает имя пользователя.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Возвращает адрес электронной почты пользователя.
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Устанавливает адрес электронной почты пользователя.
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Возвращает возраст пользователя.
-     */
-    public int getAge() {
-        return age;
-    }
-
-    /**
-     * Устанавливает возраст пользователя.
-     */
-    public void setAge(int age) {
-        this.age = age;
-    }
+        /**
+         * Возраст пользователя.
+         */
+        @Nullable
+        @Min(value = 0, message = "{min.age}") @Max(value = 120, message = "{max.age}")
+        Integer age
+) {
 }
