ce5b3d705586e6598a624fab0b5a585574d41b84
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-10 13:57:23 +0300

    feat(util): implement UserValidator with email duplication checks

---
+++ src/main/java/ru/astondevs/util/UserValidator.java
@@ -0,0 +1,26 @@
+package ru.astondevs.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.astondevs.dto.UserCreateDto;
+import ru.astondevs.dto.UserUpdateDto;
+import ru.astondevs.exception.DuplicateEmailException;
+import ru.astondevs.repository.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class UserValidator {
+    private final UserRepository userRepository;
+
+    public void validateCreateDto(UserCreateDto dto) {
+        if (userRepository.existsByEmail(dto.email())) {
+            throw new DuplicateEmailException("Email уже существует");
+        }
+    }
+
+    public void validateUpdateDto(UserUpdateDto dto) {
+        if (dto.email() != null && userRepository.existsByEmail(dto.email())) {
+            throw new DuplicateEmailException("Email уже существует");
+        }
+    }
+}
