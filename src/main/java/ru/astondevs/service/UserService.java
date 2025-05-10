feba562b0889128b0010f35107d99eb3bd35c3cc
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-10 13:53:58 +0300

    feat(service): add CRUD method signatures to UserService interface

--- src/main/java/ru/astondevs/service/UserService.java
+++ src/main/java/ru/astondevs/service/UserService.java
@@ -1,4 +1,15 @@
 package ru.astondevs.service;
 
+import java.util.List;
+import ru.astondevs.dto.UserCreateDto;
+import ru.astondevs.dto.UserResponseDto;
+import ru.astondevs.dto.UserUpdateDto;
+
+
 public interface UserService {
+    UserResponseDto createUser(UserCreateDto dto);
+    UserResponseDto getUserById(Long id);
+    List<UserResponseDto> getAllUsers();
+    UserResponseDto updateUser(Long id, UserUpdateDto dto);
+    void deleteById(Long id);
 }
