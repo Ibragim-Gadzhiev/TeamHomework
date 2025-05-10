caeb42d14f4a831cd563c43d001f2ad07258fedc
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-10 13:43:13 +0300

    chore(controller): clean up logging in UserController

--- src/main/java/ru/astondevs/controller/UserController.java
+++ src/main/java/ru/astondevs/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.astondevs.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +18,6 @@ import ru.astondevs.dto.UserResponseDto;
 import ru.astondevs.dto.UserUpdateDto;
 import ru.astondevs.service.UserService;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -29,43 +27,29 @@ public class UserController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponseDto createUser(@Valid @RequestBody UserCreateDto dto) {
-        log.info("POST /api/users: Creating user with email '{}'", dto.email());
-        UserResponseDto createdUser = userService.createUser(dto);
-        log.debug("Created user with ID: {}", createdUser.id());
-        return createdUser;
+        return userService.createUser(dto);
     }
 
     @GetMapping("/{id}")
     public UserResponseDto getUserById(@PathVariable Long id) {
-        log.info("GET /api/users/{}: Fetching user", id);
-        UserResponseDto user = userService.getUserById(id);
-        log.debug("Found user: {}", user);
-        return user;
+        return userService.getUserById(id);
     }
 
     @GetMapping
     public List<UserResponseDto> getAllUsers() {
-        log.info("GET /api/users: Fetching all users");
-        List<UserResponseDto> users = userService.getAllUsers();
-        log.debug("Found {} users", users.size());
-        return users;
+        return userService.getAllUsers();
     }
 
     @PatchMapping("/{id}")
     public UserResponseDto updateUser(
             @PathVariable Long id,
             @Valid @RequestBody UserUpdateDto dto) {
-        log.info("PATCH /api/users/{}: Updating user with data: {}", id, dto);
-        UserResponseDto updatedUser = userService.updateUser(id, dto);
-        log.debug("Updated user {}: {}", id, updatedUser);
-        return updatedUser;
+        return userService.updateUser(id, dto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Long id) {
-        log.info("DELETE /api/users/{}: Deleting user", id);
         userService.deleteById(id);
-        log.debug("User {} successfully deleted", id);
     }
 }
