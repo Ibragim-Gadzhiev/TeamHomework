4b9f1f0cd5b439200b859064b5b646262a8efe02
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-10 23:02:03 +0300

    refactor(exception): replace flat validation error map with structured response

--- src/main/java/ru/astondevs/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/astondevs/exception/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package ru.astondevs.exception;
 
-import java.util.List;
-import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
