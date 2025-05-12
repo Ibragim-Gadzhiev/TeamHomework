a26d6ff80fde425155b00cc3f5f9fedf47902d7e
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-12 13:24:05 +0300

    refactor(kafka): send structured user event as JSON instead of plain string

--- src/main/java/ru/astondevs/service/KafkaProducer.java
+++ src/main/java/ru/astondevs/service/KafkaProducer.java
@@ -1,20 +1,22 @@
 package ru.astondevs.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import ru.astondevs.dto.UserEventDto;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaProducer {
-
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public void sendUserEvent(String topic, UserEventDto event) {
+        String message = serializeEvent(event);
+        kafkaTemplate.send(topic, message);
     }
 
-    public void sendUserEvent(String topic, String email) {
-        kafkaTemplate.send(topic, email);
-        System.out.println("Sent to Kafka: " + email);
+    private String serializeEvent(UserEventDto event) {
+        return String.format("{\"operation\":\"%s\",\"email\":\"%s\"}",
+                event.operation(), event.email());
     }
-
 }
