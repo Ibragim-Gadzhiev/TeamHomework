412dfa8c8e78aea0b30b81b30ff0d54cebd8e09d
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-05-15 03:11:36 +0300

    fix(config): add setter annotation to KafkaConfig

--- common/src/main/java/ru/astondevs/config/KafkaConfig.java
+++ common/src/main/java/ru/astondevs/config/KafkaConfig.java
@@ -1,10 +1,12 @@
 package ru.astondevs.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 public class KafkaConfig {
 
